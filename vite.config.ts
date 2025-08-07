2e2d829ab4cfb0fe5855c3aa9ab49c550c33090c
AnnabelGoldman <annabel@crossplaydigital.com>
2025-08-06 17:40:01 -0700

    Adjusted config to work with custom domain.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/SydneyBrenton/' : '/',
+  base: '/',
   assetsInclude: ['**/*.mov', '**/*.mp4', '**/*.heic', '**/*.jpeg', '**/*.jpg', '**/*.png', '**/*.svg'],
 })
